7dac831d0db4e75c42112b6e6d9ba765d05cb98e
Abdulaziz Almoallim <aalmo038@uottawa.ca>
2019-10-16 16:49:42 -0400

    Simulation C Code

--- Simulation/Random.h
+++ Simulation/Random.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-extern long A;
-extern long B;
-extern long M;
-extern long SEED;
+	extern long A;
+	extern long B;
+	extern long M;
+	extern long SEED;
 
-double generateNextUniform();
-double generateNextGaussian(double mean, double var);
-int generateNextSymbol();
+	double generateNextUniform();
+	double generateNextGaussian(double mean, double var);
+	int generateNextSymbol();
--- Simulation/Task3/TAsk3.c
+++ Simulation/Task3/TAsk3.c
@@ -5,7 +5,7 @@
 int main() {
 	int rand_num = 20000;
 	double number;
-	File * file = fopen("C:\Users\aziza\OneDrive\University\Fifth Year\ELG4176\Simulation Lab\Coms_Simulation\Simulation\Task3\data\randomGaussianOut.dat", "w");
+	FILE *file = fopen("./Users/aziza/OneDrive/University/Fifth Year/ELG4176/Simulation Lab/Coms_Simulation/Simulation/data/randomGaussianOut.dat", "w");
 	for (int i = 0; i < rand_num; i++) {
 		number = generateNextGaussian(0, 1);
 		fprintf(file, "%f \n", number);
